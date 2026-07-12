57b00a50375e175b18827a823f3ea7119d2344e3
Circulate233 <a18966009058@163.com>
2026-07-12 17:32:26 +0800

    修复一下自动构建，删除不再需要的mek依赖

--- src/main/java/com/circulation/only_one_item/handler/InitHandler.java
+++ src/main/java/com/circulation/only_one_item/handler/InitHandler.java
@@ -1,22 +1,11 @@
 package com.circulation.only_one_item.handler;
 
-import com.circulation.only_one_item.util.OOIMekRecipe;
-import mekanism.common.recipe.RecipeHandler;
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.Optional;
-
 public class InitHandler {
 
     public static void allPreInit() {
         MatchFluidHandler.lock();
         MatchItemHandler.preItemStackInit();
         MatchFluidHandler.preFluidStackInit();
-
-        if (Loader.isModLoaded("mekanism")) MekInit();
     }
 
-    @Optional.Method(modid = "mekanism")
-    private static void MekInit() {
-        RecipeHandler.Recipe.values().forEach(recipe -> ((OOIMekRecipe) recipe).ooi$refresh());
-    }
 }
--- src/main/java/com/circulation/only_one_item/mixin/LateMixinLoader.java
+++ src/main/java/com/circulation/only_one_item/mixin/LateMixinLoader.java
@@ -20,7 +20,6 @@ public class LateMixinLoader implements ILateMixinLoader {
 
     static {
         addModdedMixinCFG("mixins.only_one_item.crt.json", "crafttweaker");
-        addModdedMixinCFG("mixins.only_one_item.mek.json", "mekanism");
     }
 
     private static boolean modLoaded(final String modID) {
